3837470a04e66595fd0796a2363c5149be806778
tranbuithanh <109666928+tranbuithanh@users.noreply.github.com>
2024-07-08 20:00:41 +0700

    Add files via upload

---
+++ SpringExam/src/main/java/Exam_T2208E/SpringExam/SpringExamApplication.java
@@ -0,0 +1,13 @@
+package Exam_T2208E.SpringExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExamApplication.class, args);
+	}
+
+}
---
+++ SpringExam/src/test/java/Exam_T2208E/SpringExam/SpringExamApplicationTests.java
@@ -0,0 +1,13 @@
+package Exam_T2208E.SpringExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
